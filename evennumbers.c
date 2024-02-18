771b13a5977dee759f16be4b78f7ee2c704b618f
Anuraggupta4 <142493412+Anuraggupta4@users.noreply.github.com>
2024-02-19 00:42:51 +0530

    Add files via upload

---
+++ evennumbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    int i;
+    printf("enter the and b values");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        if(i%2==0)
+        {
+           printf("%d\n\n",i);
+        }
+        
+         
+    }
+    i++;
+    }
