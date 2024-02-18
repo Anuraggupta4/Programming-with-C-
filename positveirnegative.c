03488e1068dfc77a1a8a3aeb7a5e54f49eebbb08
Anuraggupta4 <142493412+Anuraggupta4@users.noreply.github.com>
2024-02-19 00:04:03 +0530

    Add files via upload

---
+++ positveirnegative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int num=0;
+   if(num>0)
+   {
+    printf("positive number");
+   }
+   else if(num<0)
+   {
+    printf("negative number");
+   }
+   else{
+    printf("0");
+   }
+}
